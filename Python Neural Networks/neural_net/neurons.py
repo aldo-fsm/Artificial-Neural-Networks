e7c3a584ab61368cf3be5e1783da832c77fc9630
Aldo Monteiro <aldo.fsmonteiro@gmail.com>
2017-01-07 00:49:24 -0300

    classe Layer

--- Python Neural Networks/neural_net/neurons.py
+++ Python Neural Networks/neural_net/neurons.py
@@ -3,3 +3,53 @@ Created on 7 de jan de 2017
 
 @author: aldo
 '''
+
+
+class Layer:
+    # pesos das entradas
+    weights_in = {}
+    # pesos das saidas
+    weights_out = {}
+    visited = False
+    
+    def __init__(self, number_units):
+        self.size = number_units
+
+    # valores da saida atual da camada        
+    @property
+    def outputs(self):
+        if self.visited :
+            return self.outputs 
+        else :
+            '''
+        -----------------------------------------
+            calcula o output (falta implementar)
+        -----------------------------------------            
+            '''
+            return None
+    @outputs.setter
+    def outputs(self, value):
+        self.outputs = value
+        self.visited = True
+        
+    # derivada do erro em relação ao input
+    @property
+    def errors(self):
+        if self.visited :
+            return self.error
+        else :
+            '''
+        -----------------------------------------
+            calcula o erro (falta implementar)
+        -----------------------------------------            
+            '''
+            return None
+   
+    @errors.setter
+    def errors(self, value):
+        self.errors = value
+        self.visited = True
+        
+class SynapticWeights:
+    def __init__(self, weight_matrix):
+        self.matrix = weight_matrix
