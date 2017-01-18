9708241b22b6187eb2c35a9fc2a5742363203f57
Aldo Monteiro <aldo.fsmonteiro@gmail.com>
2017-01-18 14:45:59 -0300

    testes com colormap

--- Python Neural Networks/tests/neural_net_xor_test.py
+++ Python Neural Networks/tests/neural_net_xor_test.py
@@ -3,9 +3,12 @@ Created on 9 de jan de 2017
 
 @author: aldo
 '''
+
+from matplotlib import pyplot as plt
+import numpy as np
+
 from annpy.neural_nets import NeuralNetwork
 from annpy.training import DataSet
-from matplotlib import pyplot as plt
 
 
 ann = NeuralNetwork()
@@ -17,8 +20,8 @@ ann.add_layer('output', 1)
 ann.input_layers = 'input'
 ann.output_layers = 'output'
 
-ann.connect('input', 'hidden', 5)
-ann.connect('hidden', 'output', 5)
+ann.connect('input', 'hidden', 0.5)
+ann.connect('hidden', 'output', 0.5)
 
 data = DataSet()
 data.add_training_case(1, 1, 0)
@@ -35,5 +38,23 @@ print(ann.output(1, 0))
 print(ann.output(0, 1))
 print(ann.output(0, 0))
 
-plt.plot(errors)
+
+x = np.linspace(-0.5, 1.5, 50)
+y = np.linspace(-0.5, 1.5, 50)
+ 
+X, Y = np.meshgrid(x, y)
+ 
+@np.vectorize
+def f(x, y):
+    return float(ann.output(x, y)[0])
+Z = f(X, Y)
+plt.pcolormesh(X, Y, Z, cmap=plt.cm.get_cmap('RdBu'))
+plt.colorbar()
+plt.scatter([1, 0], [1, 0], marker='o', color='y')
+plt.scatter([1, 0], [0, 1], marker='x', color='y')
+plt.axis([-0.5, 1.5, -0.5, 1.5])
 plt.show()
+
+
+# plt.plot(errors)
+# plt.show()
--- Python Neural Networks/tests/single_layer_nn_test.py
+++ Python Neural Networks/tests/single_layer_nn_test.py
@@ -3,6 +3,10 @@ Created on 9 de jan de 2017
 
 @author: aldo
 '''
+
+from matplotlib import pyplot as plt
+import numpy as np
+
 from annpy.neural_nets import NeuralNetwork
 from annpy.training import DataSet
 
@@ -21,8 +25,23 @@ data.add_training_case(0, 1, 1)
 data.add_training_case(0, 0, 0)
 
 print(ann1.in_weights_of('output'))
-ann1.train(data, 100)
+ann1.train(data, 500)
 print(ann1.in_weights_of('output'))
 
 print(ann1.output([1, 1], [1, 0], [0, 1], [0, 0]))
-
+ 
+x = np.linspace(-0.5, 1.5, 50)
+y = np.linspace(-0.5, 1.5, 50)
+ 
+X, Y = np.meshgrid(x, y)
+ 
+@np.vectorize
+def f(x, y):
+    return float(ann1.output(x, y)[0])
+Z = f(X, Y)
+plt.pcolormesh(X, Y, Z, cmap=plt.cm.get_cmap('RdBu'))
+plt.colorbar()
+plt.scatter(0, 0, marker='o', color='y')
+plt.scatter([1, 1, 0], [1, 0, 1], marker='x', color='y')
+plt.axis([-0.5, 1.5, -0.5, 1.5])
+plt.show()
