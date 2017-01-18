fbe5188d60d8812c9cebee8ddb0f6c6eedf60b50
Aldo Monteiro <aldo.fsmonteiro@gmail.com>
2017-01-18 00:37:40 -0300

    testes esn

--- Python Neural Networks/tests/esn/test1.py
+++ Python Neural Networks/tests/esn/test1.py
@@ -24,10 +24,29 @@ data.add_training_case(15, 16, 17, 16, 17, 18)
  
 errors = []
 esn.train(data, 1000, error_list=errors)
- 
-print(esn.output(1))
-print(esn.output(2))
-print(esn.output(3))
+
+print(esn.ho_weights)
+
+print(esn.output(1), 2)
+print(esn.output(2), 3)
+print(esn.output(3), 4)
+print(esn.output(4), 5)
+print(esn.output(5), 6)
+print(esn.output(6), 7)
+print(esn.output(7), 8)
+print(esn.output(8), 9)
+print(esn.output(9), 10)
+print(esn.output(10), 11)
+print(esn.output(11), 12)
+print(esn.output(12), 13)
+print(esn.output(13), 14)
+print(esn.output(14), 15)
+print(esn.output(15), 16)
+print(esn.output(16), 17)
+print(esn.output(17), 18)
+print(esn.output(18), 19)
+print(esn.output(19), 20)
+print(esn.output(20), 21)
  
 plt.plot(errors)
 plt.show()
